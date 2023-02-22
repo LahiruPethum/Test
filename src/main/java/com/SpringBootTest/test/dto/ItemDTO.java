a3a6a485ec9915dbd9394f189cf6774db7aee0d8
Lahiru Pethum <pethum013@gmail.com>
2023-02-22 12:36:11 +0530

    make dto

---
+++ src/main/java/com/SpringBootTest/test/dto/ItemDTO.java
@@ -0,0 +1,24 @@
+package com.SpringBootTest.test.dto;
+
+import com.SpringBootTest.test.entity.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemDTO {
+
+    private int itemId;
+    private String itemName;
+    private Category category;
+    private String sku;
+    private String description;
+    private double itemPrice;
+    private double itemCount;
+    private Date data;
+    private boolean remarks;
+}
